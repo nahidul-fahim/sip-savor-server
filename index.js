413a0028ae24710785993da0a153f35549d3f979
nahidul-fahim <nahidulfahim.official@gmail.com>
2023-11-06 20:09:52 +0600

    added functionality to get all the food items

--- index.js
+++ index.js
@@ -11,8 +11,11 @@ app.use(express.json())
 
 
 
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+
 // Mongo DB code snippet
-const uri = "mongodb+srv://<username>:<password>@cluster0.xeklkbf.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${dbUser}:${dbPass}@cluster0.xeklkbf.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,6 +30,19 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        // Database and collection
+        const allUsers = client.db("sipSavorRestaurant").collection("users");
+        const allFoods = client.db("sipSavorRestaurant").collection("foods");
+
+        // Get all the food items
+        app.get("/allfoods", async(req, res) => {
+            const result = await allFoods.find().toArray();
+            res.send(result);
+        })
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -36,3 +52,14 @@ async function run() {
     }
 }
 run().catch(console.dir);
+
+
+// Checking if server is runnig
+app.get("/", (req, res) => {
+    res.send("Sip & Savor Restaurant is running fine")
+})
+
+// Declaring port & checking the running port
+app.listen(port, () => {
+    console.log(`Sip & Savor is running on port: ${port}`)
+})
